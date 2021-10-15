9f833aa3219fefc8bfed6dd94c63dd04b58679e2
Vinicius Almada <viniciusalmada10@gmail.com>
2021-10-15 18:17:14 -0300

    add arithmetic functions to set dialog size

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iup/iup.h>
 #include <iup/iupgl.h>
 #include <GL/glew.h>
+#include <cstdio>
+#include <string>
 
 #include "gl_utils.h"
 #include "board.h"
@@ -10,38 +12,12 @@ Board* board;
 int canvas_action_cb(Ihandle* ih, float posx, float posy)
 {
   IupGLMakeCurrent(ih);
-  GlUtils::uglClearColor(0xE8E6E4);
+  GlUtils::uglClearColor(GlUtils::WHITE);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glEnable(GL_LINE_SMOOTH);
 
-  int x = 0;
   if (board)
-    x = board->getSideSize();
-  glViewport(0, 0, x, x);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(0, x, x, 0, -1.0, 1.0);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-
-  glShadeModel(GL_SMOOTH);
-
-  glBegin(GL_TRIANGLES);
-  GlUtils::uglColor3d(0xFF0000);
-  glVertex2d(x * 0.5, x * 0.1);
-  GlUtils::uglColor3d(0x00FF00);
-  glVertex2d(x * 0.1, x * 0.9);
-  GlUtils::uglColor3d(0x0000FF);
-  glVertex2d(x * 0.9, x * 0.9);
-  glEnd();
-
-   glBegin(GL_QUADS);
-   GlUtils::uglColor3d(0x314FAB);
-   glVertex2d(x * 0.3, x * 0.3);
-   glVertex2d(x * 0.6, x * 0.3);
-   glVertex2d(x * 0.6, x * 0.6);
-   glVertex2d(x * 0.3, x * 0.6);
-   glEnd();
+    board->drawBoard();
 
   IupGLSwapBuffers(ih);
 
@@ -50,36 +26,52 @@ int canvas_action_cb(Ihandle* ih, float posx, float posy)
 
 int redraw_canvas_cb(Ihandle* ih)
 {
-  IupRedraw(IupGetHandle("canvas"), true);
+  printf(" called ");
+  IupRedraw(ih, true);
 
   return IUP_DEFAULT;
 }
 
 int main(int argc, char* argv[])
 {
+  int x = 600;
+  board = new Board(x);
+
   IupOpen(&argc, &argv);
   IupGLCanvasOpen();
 
   Ihandle* canvas = IupGLCanvas(nullptr);
   IupSetHandle("canvas", canvas);
   IupSetAttribute(canvas, "EXPAND", "YES");
-  IupSetAttribute(canvas, "BORDERS", "NO");
+  IupSetAttribute(canvas, "BORDERS", "YES");
+  IupSetCallback(canvas, "K_s", redraw_canvas_cb);
   IupSetCallback(canvas, "ACTION", (Icallback)canvas_action_cb);
 
   Ihandle* box = IupHbox(canvas, nullptr);
 
   Ihandle* dlg = IupDialog(box);
   IupSetAttribute(dlg, "TITLE", "Chess with IUP");
-  IupSetAttribute(dlg, "RASTERSIZE", "600x600");
+  IupSetAttribute(dlg, "RASTERSIZE", "100x100"); // 16 hor / 39 ver
   IupSetAttribute(dlg, "RESIZE", "NO");
-  IupSetCallback(dlg, "K_s", redraw_canvas_cb);
 
   IupShowXY(dlg, IUP_CENTER, IUP_CENTER);
 
-  int x;
-  IupGetIntInt(canvas, "DRAWSIZE", &x, nullptr);
-  board = new Board(x);
+  int ndx, ndy;
+  IupGetIntInt(dlg, "NATURALSIZE", &ndx, &ndy);
+  int cdx, cdy;
+  IupGetIntInt(dlg, "CLIENTSIZE", &cdx, &cdy);
 
+  int userPredefined = 600;
+  int dx = ndx - cdx;
+  int dy = ndy - cdy;
+  int newX = userPredefined + dx;
+  int newY = userPredefined + dy;
+  std::string str = std::to_string(newX) + "x" + std::to_string(newY);
+  IupHide(dlg);
+  IupSetAttribute(dlg, "RASTERSIZE", str.c_str());
+  IupShowXY(dlg, IUP_CENTER, IUP_CENTER);
+
+  IupRedraw(canvas, true);
   IupRedraw(canvas, true);
 
   IupMainLoop();
