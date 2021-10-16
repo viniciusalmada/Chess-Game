0b2ce1b4945e3d84168b5198f34795fd41ddb4e3
Vinicius Almada <viniciusalmada10@gmail.com>
2021-10-16 14:59:34 -0300

    create App class to holds global information

---
+++ include/app.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+class App
+{
+  std::string mImagesPath;
+
+public:
+  App(std::string executablePath);
+};
---
+++ src/app.cpp
@@ -0,0 +1,6 @@
+#include "app.h"
+
+App::App(std::string executablePath)
+{
+
+}
