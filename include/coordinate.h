56d6594c868bc6ff3d3931671932ee0c53915e8f
Vinicius Almada <viniciusalmada10@gmail.com>
2021-10-17 11:35:53 -0300

    create Coordinate class

---
+++ include/coordinate.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <utility>
+
+class Coordinate
+{
+  std::pair<int, int> pair;
+
+public:
+
+  Coordinate(int a, int b);
+};
---
+++ src/coordinate.cpp
@@ -0,0 +1,6 @@
+#include "..\include\coordinate.h"
+
+Coordinate::Coordinate(int a, int b)
+{
+  pair = { a, b };
+}
