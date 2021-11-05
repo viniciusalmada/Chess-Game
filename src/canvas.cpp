8aa9b215d4b0b66cb48a4c53e700b1851b480f26
Vinicius Almada <viniciusalmada10@gmail.com>
2021-11-05 01:53:10 -0300

    create call to draw triangle in modern OpenGL

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -5,6 +5,7 @@
 #include <gl_utils.h>
 #include <GL/glew.h>
 #include <log.h>
+#include <iostream>
 
 std::string Canvas::HANDLE_NAME = "CHESS_CANVAS_HANDLE_NAME";
 
@@ -13,13 +14,9 @@ static Coordinate _mousePressedCoord;
 int Canvas::actionCallback(Ihandle* cnv)
 {
   IupGLMakeCurrent(cnv);
-  GlUtils::uglClearColor(GlUtils::WHITE);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-  App::updateBoard();
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 
   IupGLSwapBuffers(cnv);
 
@@ -56,3 +53,28 @@ void Canvas::init()
   IupSetCallback(mCnv, IUP_ACTION, (Icallback)Canvas::actionCallback);
   IupSetCallback(mCnv, IUP_BUTTON_CB, (Icallback)Canvas::buttonCallback);
 }
+
+void Canvas::initOGL() const
+{
+  IupGLMakeCurrent(mCnv);
+
+  if (glewInit())
+    std::cerr << "Error!" << std::endl;
+
+  float positions[] = {
+    -0.5f, -0.5f,
+    0.0f, 0.5f,
+    0.5f, -0.5f
+  };
+
+  unsigned int bufferID;
+  glGenBuffers(1, &bufferID);
+  glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+  
+
+  glEnableVertexAttribArray(0); // positions attribute
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+ }
