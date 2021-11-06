51228803ab8f32349df50763fb466fd830ae7bc9
Vinicius Almada <viniciusalmada10@gmail.com>
2021-11-06 01:05:04 -0300

    add code to compile shader and shows a triangle

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -6,10 +6,52 @@
 #include <GL/glew.h>
 #include <log.h>
 #include <iostream>
+#include <string>
 
 std::string Canvas::HANDLE_NAME = "CHESS_CANVAS_HANDLE_NAME";
 
 static Coordinate _mousePressedCoord;
+
+static unsigned int CompileShader(unsigned int type, const std::string& src)
+{
+  unsigned int id = glCreateShader(type);
+  auto sourceC = src.c_str();
+  glShaderSource(id, 1, &sourceC, nullptr);
+  glCompileShader(id);
+
+  int result;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+  if (!result)
+  {
+    int len;
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
+    char* message = new char[len];
+    glGetShaderInfoLog(id, len, &len, message);
+    std::cout << message << std::endl;
+    glDeleteShader(id);
+    delete[] message;
+    return 0;
+  }
+
+  return id;
+}
+
+static unsigned int CreateProgram(const std::string& vertexShader, const std::string& fragShader)
+{
+  unsigned int programId = glCreateProgram();
+  unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+  unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragShader);
+
+  glAttachShader(programId, vs);
+  glAttachShader(programId, fs);
+  glLinkProgram(programId);
+  glValidateProgram(programId);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return programId;
+}
 
 int Canvas::actionCallback(Ihandle* cnv)
 {
@@ -61,20 +103,43 @@ void Canvas::initOGL() const
   if (glewInit())
     std::cerr << "Error!" << std::endl;
 
+  std::string vshader = R"glsl(
+    #version 410 core
+    layout(location = 0) in vec4 position;
+    layout(location = 1) in vec3 rgbColor;
+    out vec4 fragColor;
+    void main()
+    {
+      gl_Position = position;
+      fragColor = vec4(rgbColor.xyz, 1.0);
+    }
+    )glsl";
+  std::string fshader = R"glsl(
+    #version 410 core
+    in vec4 fragColor;
+    out vec4 color;
+    void main()
+    {
+      color = fragColor;
+    }
+    )glsl";
+
   float positions[] = {
-    -0.5f, -0.5f,
-    0.0f, 0.5f,
-    0.5f, -0.5f
+    -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+    0.0f, 0.5f, 0.0f, 1.0f, 0.0f,
+    0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
   };
 
   unsigned int bufferID;
   glGenBuffers(1, &bufferID);
   glBindBuffer(GL_ARRAY_BUFFER, bufferID);
-  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-  
+  glBufferData(GL_ARRAY_BUFFER, 15 * sizeof(float), positions, GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0); // positions attribute
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
 
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
- }
+  unsigned int program = CreateProgram(vshader, fshader);
+  glUseProgram(program);
+}
